b4ad554912af78360ebc33de377a0048d761cbd6
Micah Godbolt <mgodbolt@phase2technology.com>
2014-11-06 09:21:37 -0800

    New Susy demo and popular video overlay

--- sass/components/popular-video/tests/popular-video-test.js
+++ sass/components/popular-video/tests/popular-video-test.js
@@ -10,4 +10,8 @@ casper.thenOpen('http://localhost:9001/build/styleguide/component_-_popular_vide
     .then(function () {
         this.viewport(1000, 1000);
         phantomcss.screenshot('.popular-video', 'popular-video-desktop');
+    })
+    .then(function () {
+        this.mouse.move(".popular-video-img-container"); // Trigger :hover state
+        phantomcss.screenshot('.popular-video', 'popular-video-desktop-img-hover');
     });
